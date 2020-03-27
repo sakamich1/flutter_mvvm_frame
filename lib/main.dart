a1ffed85cd4a9a76eeb25a7ea08831ddf991a252
sakamichi <sakamichi9158@gmail.com>
2020-03-27 19:52:14 +0800

    immersion status bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mvvm_frame/global/dependencies_module.dart';
 import 'package:flutter_mvvm_frame/module/view/home.dart';
 import 'global/routes.dart';
@@ -7,6 +10,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await init();
   runApp(MyApp());
+  if (Platform.isAndroid) {
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    SystemUiOverlayStyle systemUiOverlayStyle =
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
 
 class MyApp extends StatelessWidget {
